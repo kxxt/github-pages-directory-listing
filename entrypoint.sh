65d993f5cc9613f50dd7f0b277af45b7f83931a8
Levi Zim <rsworktech@outlook.com>
2023-02-27 18:52:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install cmake python3 -y
-git clone --depth 1 https://github.com/jayanta525/apindex-v2.git
+git clone --depth 1 https://github.com/kxxt/apindex-v2.git
 cd apindex-v2
 cmake . -DCMAKE_INSTALL_PREFIX=/usr
 sudo make install
